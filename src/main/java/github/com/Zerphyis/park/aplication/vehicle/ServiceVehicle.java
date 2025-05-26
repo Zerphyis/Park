e4af2854ea457db53b3d721994377451fc3df78f
Otavio <estudosprogotavio@gmail.com>
2025-05-26 03:39:28 -0300

    Feat: service de Vehicle

---
+++ src/main/java/github/com/Zerphyis/park/aplication/vehicle/ServiceVehicle.java
@@ -0,0 +1,41 @@
+package github.com.Zerphyis.park.aplication.vehicle;
+
+import github.com.Zerphyis.park.domain.VehicleNotFound;
+import github.com.Zerphyis.park.domain.vehicle.DataVehicle;
+import github.com.Zerphyis.park.infra.vehicle.RepositoryVehicle;
+import github.com.Zerphyis.park.infra.vehicle.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceVehicle {
+    @Autowired
+    RepositoryVehicle repository;
+
+    public Vehicle registerVehicle(DataVehicle data){
+        var newVehicle= new Vehicle(data);
+       return repository.save(newVehicle);
+    }
+
+    public List<Vehicle> listVehicle(){
+        return repository.findAll();
+    }
+
+    public Vehicle updateveihcle(Long id,DataVehicle data){
+        var vehicle = repository.findById(id)
+                .orElseThrow(() -> new VehicleNotFound("Veículo com id " + id + " não encontrado."));
+
+        vehicle.setTypeClient(data.typeClient());
+
+        return repository.save(vehicle);
+    }
+
+    public void deleteVehicle(Long id) {
+        if (!repository.existsById(id)) {
+            throw new VehicleNotFound("Veículo com id " + id + " não encontrado.");
+        }
+        repository.deleteById(id);
+    }
+}
