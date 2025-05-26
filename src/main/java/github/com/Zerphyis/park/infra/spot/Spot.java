d67a6d259897575198aa488b2817ea698764c746
Otavio <estudosprogotavio@gmail.com>
2025-05-26 05:16:49 -0300

    feat:adicionando entidade Spot

---
+++ src/main/java/github/com/Zerphyis/park/infra/spot/Spot.java
@@ -0,0 +1,47 @@
+package github.com.Zerphyis.park.infra.spot;
+
+
+import github.com.Zerphyis.park.domain.spot.DataSpot;
+import github.com.Zerphyis.park.domain.spot.TypeSpot;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name ="vaga" )
+public class Spot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message ="número da vaga deve estar preenchido")
+    private Integer numberPark;
+    @Enumerated(EnumType.STRING)
+    private TypeSpot typeSpot;
+
+    public Spot(){
+
+
+    }
+
+    public  Spot(DataSpot data){
+        this.numberPark= data.numberPark();
+        this.typeSpot=data.typeSpot();
+
+    }
+
+    public Integer getNumberPark() {
+        return numberPark;
+    }
+
+    public void setNumberPark(Integer numberPark) {
+        this.numberPark = numberPark;
+    }
+
+    public TypeSpot getTypeSpot() {
+        return typeSpot;
+    }
+
+    public void setTypeSpot(TypeSpot typeSpot) {
+        this.typeSpot = typeSpot;
+    }
+}
