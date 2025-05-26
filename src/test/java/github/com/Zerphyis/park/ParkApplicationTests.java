7403c5490227b5c86a3add0735f9953e932cc7bf
Otavio <estudosprogotavio@gmail.com>
2025-05-26 03:37:24 -0300

    Feat:commit inicial

---
+++ src/test/java/github/com/Zerphyis/park/ParkApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.Zerphyis.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
