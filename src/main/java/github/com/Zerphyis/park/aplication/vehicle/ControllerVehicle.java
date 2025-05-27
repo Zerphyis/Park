dbcc555413f54cf096d2b3e84355745824d6772f
Otavio <estudosprogotavio@gmail.com>
2025-05-27 15:57:46 -0300

    refactor:adicionando código http certo na ControllerVehicle

--- src/main/java/github/com/Zerphyis/park/aplication/vehicle/ControllerVehicle.java
+++ src/main/java/github/com/Zerphyis/park/aplication/vehicle/ControllerVehicle.java
@@ -3,33 +3,41 @@ package github.com.Zerphyis.park.aplication.vehicle;
 import github.com.Zerphyis.park.domain.vehicle.DataVehicle;
 import github.com.Zerphyis.park.infra.vehicle.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vehicle")
+@RequestMapping("/veiculos")
 public class ControllerVehicle {
     @Autowired
     ServiceVehicle service;
 
-    @PostMapping
-    public Vehicle register(@RequestBody DataVehicle data){
-        return service.registerVehicle(data);
+    public ResponseEntity<Vehicle> register(@RequestBody DataVehicle data, UriComponentsBuilder uriBuilder) {
+        Vehicle created = service.registerVehicle(data);
+        URI uri = uriBuilder.path("/veiculos/{id}").buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uri).body(created);
     }
 
+
     @PutMapping
-    public Vehicle update(@RequestParam("id") Long id,@RequestBody DataVehicle data){
-        return service.updateveihcle(id,data);
+    public ResponseEntity<Vehicle> update(@RequestParam("id") Long id, @RequestBody DataVehicle data){
+        var update=service.updateveihcle(id,data);
+        return  ResponseEntity.ok(update);
     }
 
     @GetMapping
-    public List<Vehicle> list(){
-        return service.listVehicle();
+    public ResponseEntity<List<Vehicle>> list(){
+        var list=service.listVehicle();
+        return ResponseEntity.ok(list);
     }
 
     @DeleteMapping
-    public void delete(@RequestParam("id")Long id){
+    public ResponseEntity<Void> delete(@RequestParam("id")Long id){
         service.deleteVehicle(id);
+        return ResponseEntity.noContent().build();
     }
 }
