52553a3ee26dd772217f99fbda2813ad88aeb5e4
Otavio <estudosprogotavio@gmail.com>
2025-05-27 16:04:57 -0300

    refactor:adicionando Dto de resposta na ControllerVehicle

--- src/main/java/github/com/Zerphyis/park/aplication/vehicle/ControllerVehicle.java
+++ src/main/java/github/com/Zerphyis/park/aplication/vehicle/ControllerVehicle.java
@@ -1,6 +1,7 @@
 package github.com.Zerphyis.park.aplication.vehicle;
 
 import github.com.Zerphyis.park.domain.vehicle.DataVehicle;
+import github.com.Zerphyis.park.domain.vehicle.ResponseDataVehicle;
 import github.com.Zerphyis.park.infra.vehicle.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,28 +17,35 @@ public class ControllerVehicle {
     @Autowired
     ServiceVehicle service;
 
-    public ResponseEntity<Vehicle> register(@RequestBody DataVehicle data, UriComponentsBuilder uriBuilder) {
+    @PostMapping
+    public ResponseEntity<ResponseDataVehicle> register(@RequestBody DataVehicle data, UriComponentsBuilder uriBuilder) {
         Vehicle created = service.registerVehicle(data);
         URI uri = uriBuilder.path("/veiculos/{id}").buildAndExpand(created.getId()).toUri();
-        return ResponseEntity.created(uri).body(created);
+        return ResponseEntity.created(uri).body(toResponse(created));
     }
 
-
     @PutMapping
-    public ResponseEntity<Vehicle> update(@RequestParam("id") Long id, @RequestBody DataVehicle data){
-        var update=service.updateveihcle(id,data);
-        return  ResponseEntity.ok(update);
+    public ResponseEntity<ResponseDataVehicle> update(@RequestParam("id") Long id, @RequestBody DataVehicle data){
+        Vehicle updated = service.updateveihcle(id, data);
+        return ResponseEntity.ok(toResponse(updated));
     }
 
     @GetMapping
-    public ResponseEntity<List<Vehicle>> list(){
-        var list=service.listVehicle();
+    public ResponseEntity<List<ResponseDataVehicle>> list(){
+        List<ResponseDataVehicle> list = service.listVehicle()
+                .stream()
+                .map(this::toResponse)
+                .toList();
         return ResponseEntity.ok(list);
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> delete(@RequestParam("id")Long id){
+    public ResponseEntity<Void> delete(@RequestParam("id") Long id){
         service.deleteVehicle(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private ResponseDataVehicle toResponse(Vehicle v) {
+        return new ResponseDataVehicle(v.getCarPlate(), v.getTypeClient());
     }
 }
