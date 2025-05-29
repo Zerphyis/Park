a420e8f5d27143b049c61bf9cbb4e49d1d9ffe8c
Otavio <estudosprogotavio@gmail.com>
2025-05-29 07:53:53 -0300

    refactor:removendo imports

--- src/main/java/github/com/Zerphyis/park/domain/exit/RepositoryExit.java
+++ src/main/java/github/com/Zerphyis/park/domain/exit/RepositoryExit.java
@@ -1,9 +1,10 @@
 package github.com.Zerphyis.park.domain.exit;
 
+import github.com.Zerphyis.park.domain.entry.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositoryExit extends JpaRepository<Exit,Long> {
-    boolean existsByEntryId(Long entryId);
+    boolean existsByEntry(Long entryid);
 }
