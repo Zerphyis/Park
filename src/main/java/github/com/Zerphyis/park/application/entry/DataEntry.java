500d0de3cfc3eef23bb522d15f04786dc98900a7
Otavio <estudosprogotavio@gmail.com>
2025-05-29 07:18:48 -0300

    refactor:removendo o EntryDateTime do record DataEntry

--- src/main/java/github/com/Zerphyis/park/application/entry/DataEntry.java
+++ src/main/java/github/com/Zerphyis/park/application/entry/DataEntry.java
@@ -3,7 +3,6 @@ package github.com.Zerphyis.park.application.entry;
 import github.com.Zerphyis.park.domain.spot.Spot;
 import github.com.Zerphyis.park.domain.vehicle.Vehicle;
 
-import java.time.LocalDateTime;
 
-public record DataEntry(Spot spot, Vehicle vehicle, LocalDateTime entryDateTime) {
+public record DataEntry(Spot spot, Vehicle vehicle) {
 }
