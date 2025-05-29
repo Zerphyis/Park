9fa2e2b341ef9c7e580211e3c8d6307db7521329
Otavio <estudosprogotavio@gmail.com>
2025-05-29 07:54:16 -0300

    refactor:adicionando novo atributo a entidade Exit

--- src/main/java/github/com/Zerphyis/park/domain/exit/Exit.java
+++ src/main/java/github/com/Zerphyis/park/domain/exit/Exit.java
@@ -4,6 +4,7 @@ import github.com.Zerphyis.park.application.exit.DataExit;
 import github.com.Zerphyis.park.domain.entry.Entry;
 import jakarta.persistence.*;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -20,12 +21,30 @@ public class Exit {
     @Column(name = "saida_data_hora", nullable = false)
     private LocalDateTime exitDateTime;
 
+    @Column(name = "valor_cobrado",nullable = false)
+    private Double valueCharged;
+
     public Exit(){
 
     }
     public Exit(DataExit data){
         this.entry=data.entry();
         this.exitDateTime=LocalDateTime.now();
+        this.valueCharged = calculateValue(entry.getEntryDateTime(), this.exitDateTime);
+    }
+
+
+    private double calculateValue(LocalDateTime entry, LocalDateTime exit) {
+        long minutos = Duration.between(entry, exit).toMinutes();
+        return minutos * 0.25; //R$ 0,25 por minuto
+    }
+
+    public Double getValueCharged() {
+        return valueCharged;
+    }
+
+    public void setValueCharged(Double valueCharged) {
+        this.valueCharged = valueCharged;
     }
 
     public Long getId() {
