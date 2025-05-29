eecd7284055c5b9b14bfb584b5dfce925414580f
Otavio <estudosprogotavio@gmail.com>
2025-05-29 07:21:08 -0300

    feat:Adicionando Repositorio de Exit

---
+++ src/main/java/github/com/Zerphyis/park/domain/exit/RepositoryExit.java
@@ -0,0 +1,9 @@
+package github.com.Zerphyis.park.domain.exit;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryExit extends JpaRepository<Exit,Long> {
+    boolean existsByEntryId(Long entryId);
+}
