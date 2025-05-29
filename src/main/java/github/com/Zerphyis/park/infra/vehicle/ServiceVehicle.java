451a1631543a3ce4a9aee15c275b53b1dc298128
Otavio <estudosprogotavio@gmail.com>
2025-05-29 07:20:04 -0300

    refactor:adicionando o @Transactional em ServiceVehicle

--- src/main/java/github/com/Zerphyis/park/infra/vehicle/ServiceVehicle.java
+++ src/main/java/github/com/Zerphyis/park/infra/vehicle/ServiceVehicle.java
@@ -6,6 +6,7 @@ import github.com.Zerphyis.park.domain.vehicle.RepositoryVehicle;
 import github.com.Zerphyis.park.domain.vehicle.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,15 +15,18 @@ public class ServiceVehicle {
     @Autowired
     RepositoryVehicle repository;
 
+    @Transactional
     public Vehicle registerVehicle(DataVehicle data){
         var newVehicle= new Vehicle(data);
        return repository.save(newVehicle);
     }
 
+    @Transactional(readOnly = true)
     public List<Vehicle> listVehicle(){
         return repository.findAll();
     }
 
+    @Transactional
     public Vehicle updateveihcle(Long id,DataVehicle data){
         var vehicle = repository.findById(id)
                 .orElseThrow(() -> new VehicleNotFound("Veículo com id " + id + " não encontrado."));
@@ -32,6 +36,7 @@ public class ServiceVehicle {
         return repository.save(vehicle);
     }
 
+    @Transactional
     public void deleteVehicle(Long id) {
         if (!repository.existsById(id)) {
             throw new VehicleNotFound("Veículo com id " + id + " não encontrado.");
