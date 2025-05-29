f57f99538006be52752c73dbacedf38c445e0f00
Otavio <estudosprogotavio@gmail.com>
2025-05-29 07:52:42 -0300

    refactor:adicionando modificador private em ControllerEntry

--- src/main/java/github/com/Zerphyis/park/infra/entry/ControllerEntry.java
+++ src/main/java/github/com/Zerphyis/park/infra/entry/ControllerEntry.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequestMapping("/entrada")
 public class ControllerEntry {
     @Autowired
-    ServiceEntry service;
+   private ServiceEntry service;
 
     @PostMapping
     public ResponseEntity<DataEntryResponse> createEntry(@RequestBody DataEntryRequest request) {
