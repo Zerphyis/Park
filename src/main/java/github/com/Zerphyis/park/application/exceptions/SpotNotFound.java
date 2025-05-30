8e944c9f76e165d6cd4937726dfdab38b611577a
Otavio <estudosprogotavio@gmail.com>
2025-05-30 20:18:49 -0300

    refactor:movendo SpotNotFound para um novo pacote

--- src/main/java/github/com/Zerphyis/park/application/exceptions/SpotNotFound.java
+++ src/main/java/github/com/Zerphyis/park/application/exceptions/SpotNotFound.java
@@ -1,4 +1,4 @@
-package github.com.Zerphyis.park.application;
+package github.com.Zerphyis.park.application.exceptions;
 
 public class SpotNotFound extends RuntimeException {
     public SpotNotFound(String message) {
