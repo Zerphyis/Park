0dba31f829abfb7a77d22cd41e4a28318c55026d
Otavio <estudosprogotavio@gmail.com>
2025-05-30 20:19:55 -0300

    refactor:movendo ExitAlreadyExist para um novo pacote

--- src/main/java/github/com/Zerphyis/park/application/exceptions/ExitAlreadyExist.java
+++ src/main/java/github/com/Zerphyis/park/application/exceptions/ExitAlreadyExist.java
@@ -1,4 +1,4 @@
-package github.com.Zerphyis.park.application;
+package github.com.Zerphyis.park.application.exceptions;
 
 public class ExitAlreadyExist extends RuntimeException {
     public ExitAlreadyExist(String message) {
